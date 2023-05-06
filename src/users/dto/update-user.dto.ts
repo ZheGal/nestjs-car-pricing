dd6db1146c7d1612ee953aff6310db55540aba91
Evgeniy Galkovskiy <zhegal@gmail.com>
2023-05-06 12:59:46 +0300

    lf eol

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,10 @@
-import { IsEmail, IsString, MaxLength, IsOptional, MinLength } from 'class-validator';
+import {
+  IsEmail,
+  IsString,
+  MaxLength,
+  IsOptional,
+  MinLength,
+} from 'class-validator';
 
 export class UpdateUserDto {
   @IsEmail()
