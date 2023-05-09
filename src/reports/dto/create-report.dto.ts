97872fa9a8bffbf2504f7b41551a12efacce0059
Evgeniy Galkovskiy <zhegal@gmail.com>
2023-05-09 23:14:02 +0300

    create-report-dto

---
+++ src/reports/dto/create-report.dto.ts
@@ -0,0 +1,39 @@
+import {
+  IsLatitude,
+  IsLongitude,
+  IsNumber,
+  IsString,
+  Max,
+  Min,
+} from 'class-validator';
+
+const currentYear: number = new Date().getFullYear();
+
+export class CreateReportDto {
+  @IsString()
+  make: string;
+
+  @IsString()
+  model: string;
+
+  @IsNumber()
+  @Min(1930)
+  @Max(currentYear)
+  year: number;
+
+  @IsLongitude()
+  lng: number;
+
+  @IsLatitude()
+  lat: number;
+
+  @IsNumber()
+  @Min(0)
+  @Max(1000000)
+  mileage: number;
+
+  @IsNumber()
+  @Min(0)
+  @Max(1000000)
+  price: number;
+}
--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { ReportsService } from './reports.service';
+import { CreateReportDto } from './dto/create-report.dto';
 
 @Controller('reports')
 export class ReportsController {
   constructor(private readonly reportsService: ReportsService) {}
+
+  @Post()
+  createReport(@Body() body: CreateReportDto) {
+    return body;
+  }
 }
