db079190986efced43c0001588a12b383af4b0ea
Evgeniy Galkovskiy <zhegal@gmail.com>
2023-05-10 21:51:10 +0300

    report approval

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateReportDto } from './dto/create-report.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -14,6 +14,18 @@ export class ReportsService {
   create(reportDto: CreateReportDto, user: User): Promise<Report> {
     const report = this.reportsRepository.create(reportDto);
     report.user = user;
+    return this.reportsRepository.save(report);
+  }
+
+  async changeApproval(id: string, approved: boolean) {
+    const report = await this.reportsRepository.findOne({
+      where: { id: parseInt(id) },
+    });
+    if (!report) {
+      throw new NotFoundException('Report not found');
+    }
+
+    report.approved = approved;
     return this.reportsRepository.save(report);
   }
 }
